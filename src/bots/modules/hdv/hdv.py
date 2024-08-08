d44520209e05ac2557d307977d41089161d4efbf
Valentin Alix <valentin-alix.850@outlook.fr>
2024-08-08 19:27:27 +0200

    remove default sellable

--- src/bots/modules/hdv/hdv.py
+++ src/bots/modules/hdv/hdv.py
@@ -3,7 +3,6 @@ from logging import Logger
 
 from src.bots.modules.hdv.craft import Crafter
 from src.bots.modules.hdv.sell import Seller
-from src.services.item import ItemService
 from src.services.recipe import RecipeService
 from src.services.session import ServiceSession
 from src.states.character_state import CharacterState
@@ -29,21 +28,14 @@ class Hdv:
             [elem.id for elem in recipes],
             self.character_state.character.id,
         )
-
         if len(recipes) > 0:
             self.logger.info(f"Gonna craft : {recipes}")
             self.crafter.run_crafter(recipes)
 
-        if len(self.character_state.character.sell_items) == 0:
-            # do not sell items that are in ingredient of craft items
-            sell_items = ItemService.get_default_sellable_items(
-                self.service,
-                self.character_state.character.id,
-                [_elem.id for _elem in recipes],
-            )
-        else:
-            sell_items = self.character_state.character.sell_items
-
+        sell_items = list(
+            set(self.character_state.character.sell_items)
+            & set(self.character_state.character.bank_items)
+        )
         if len(sell_items) > 0:
             self.logger.info(f"Gonna sell : {sell_items}")
             self.seller.run_seller(sell_items)
